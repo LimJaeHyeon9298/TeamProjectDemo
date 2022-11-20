4378392db24e3768c118fd8aa5c7b52d15ca4a99
h-suo <phstoo1021@naver.com>
2022-11-20 15:30:21 +0900

    ui setting

--- weatherProject/ViewController.swift
+++ weatherProject/ViewController.swift
@@ -11,6 +11,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var firstview: UIView!
     @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var tempLYView: UIView!
+    @IBOutlet weak var calenderView: UIView!
+    @IBOutlet weak var otherOptionView: UIView!
+    @IBOutlet weak var regionView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +29,11 @@ class ViewController: UIViewController {
     
     func setupUI() {
         firstview.backgroundColor = .gray
-        firstview.layer.cornerRadius = 8
+        firstview.layer.cornerRadius = 10
+        tempLYView.layer.cornerRadius = 10
+        calenderView.layer.cornerRadius = 10
+        otherOptionView.layer.cornerRadius = 10
+        regionView.layer.cornerRadius = 10
     }
     
     @objc func firstViewTapped(_ sender: UITapGestureRecognizer) {
