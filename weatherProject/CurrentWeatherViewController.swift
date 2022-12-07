1e2ade7ea5425c8226153a108d200fab9b147a1a
h-suo <phstoo1021@naver.com>
2022-12-07 10:13:56 +0900

    searchbar Update

--- weatherProject/CurrentWeatherViewController.swift
+++ weatherProject/CurrentWeatherViewController.swift
@@ -6,6 +6,9 @@ import UIKit
 
 class CurrentWeatherViewController: UIViewController, UISearchBarDelegate {
     
+    //가장 기본 뷰
+    @IBOutlet weak var backgroundView: UIView!
+    
     //searchBar
     @IBOutlet weak var searchBar: UISearchBar!
     //현재 날씨
@@ -25,20 +28,23 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate {
         setupUI()
         
         searchBar.delegate = self
+        //기본뷰 터치가 가능하도록 설정
+        self.backgroundView.isUserInteractionEnabled = true
+        //기본뷰 액션 추가
+        self.backgroundView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.backgroundViewTapped)))
     }
-    
+    //view세팅
     func setupUI() {
         weatherView.layer.cornerRadius = 15
         searchBar.layer.cornerRadius = 15
         
     }
-    
+    //back버튼을 눌렀을때
     @IBAction func backButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
-    
-    
-    private func dismissKeyboard() {
+    //기본뷰를 눌렀을때 키보드가 내려가도록 설정
+    @objc func backgroundViewTapped() {
         searchBar.resignFirstResponder()
     }
     
@@ -46,7 +52,7 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate {
         //검색시작
         
         //키보드가 올라와 있을 때, 내려가도록 설정
-        dismissKeyboard()
+        searchBar.resignFirstResponder()
         
         //검색어가 있는지 확인
         guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
