b95b733659a95ba22b61c70e5b13e1aabfbfbac8
h-suo <phstoo1021@naver.com>
2022-12-07 19:01:46 +0900

    add collectionView

--- weatherProject/CurrentWeatherViewController.swift
+++ weatherProject/CurrentWeatherViewController.swift
@@ -4,7 +4,7 @@
 
 import UIKit
 
-class CurrentWeatherViewController: UIViewController, UISearchBarDelegate {
+class CurrentWeatherViewController: UIViewController, UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
     
     //가장 기본 뷰
     @IBOutlet weak var backgroundView: UIView!
@@ -22,12 +22,18 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var weatherMinTempLabel: UILabel!
     //페이지 컨트롤
     @IBOutlet weak var weatherPageControl: UIPageControl!
+    @IBOutlet weak var weatherCollectionView: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        
+        //searchBar 델리게이트 설정
         searchBar.delegate = self
+        //collectionView 델리게이트 설정
+        weatherCollectionView.delegate = self
+        weatherCollectionView.dataSource = self
+        
         //기본뷰 터치가 가능하도록 설정
         self.backgroundView.isUserInteractionEnabled = true
         //기본뷰 액션 추가
@@ -57,5 +63,15 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate {
         //검색어가 있는지 확인
         guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
         print("검색어: \(searchTerm)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 27
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = weatherCollectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCollectionViewCell", for: indexPath) as! WeatherCollectionViewCell
+        
+        return cell
     }
 }
