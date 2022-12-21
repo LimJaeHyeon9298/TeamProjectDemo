86ee55b4f1fdf5106e7589b632ff19e43a07d659
h-suo <phstoo1021@naver.com>
2022-12-21 19:00:51 +0900

    commit

--- weatherProject/CurrentWeatherViewController.swift
+++ weatherProject/CurrentWeatherViewController.swift
@@ -40,6 +40,9 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate, UICol
     
     //바람
     @IBOutlet weak var windView: UIView!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var windDirectionLabel: UILabel!
+    @IBOutlet weak var windExLabel: UILabel!
     
     //체감온도
     @IBOutlet weak var apparentTemperatureView: UIView!
@@ -59,6 +62,8 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate, UICol
     //테이블뷰
     @IBOutlet weak var weekWeatherTableView: UITableView!
     
+    //날씨 컨디션 저장
+    var currentWeatherCondition = ""
     //시간당 온도
     var hourWeatherTempArray: [String] = []
     var hourWeatherSymbol: [String] = []
@@ -76,12 +81,14 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate, UICol
     //자외선 지수
     var currentWeatherUvIndex: Int = 0
     //풍속, 풍향
-    var currentWeatherWindSpeed = ""
+    var currentWeatherWindSpeed: Int = 0
     var currentWeatherWinddirection = ""
     //체감온도
     var currentWeatherApparentTemperature: Int = 0
     //습도
     var currentWeatherHumidity: Int = 0
+    //이슬점
+    var currentWeatherDewPoint: Int = 0
     //강수량
     var precipitation: Int = 0
     
@@ -129,6 +136,7 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate, UICol
     //현재 온도 세팅
     private func setWeatherUI() {
         //현재 날씨 뷰 세팅
+        weatherLabel.text = currentWeatherCondition
         weatherTempLabel.text = "\(self.currentWeatherTemp)º"
         weatherMaxTempLabel.text = "최고:\(self.dailyWeatherMaxTemp)º"
         weatherMinTempLabel.text = "최저:\(self.dailyWeatherMinTemp)º"
@@ -137,13 +145,23 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate, UICol
         uvIndexLabel.text = "\(currentWeatherUvIndex)"
         uvIndexProgressView.progress = 0.08 * Float(currentWeatherUvIndex)
         uvIndexExLabel.numberOfLines = 2
+        uvIndexProgressSetup()
         //가시거리 세팅
         visibilityLabel.text = "\(currentWeatherVisibility)km"
+        visibilityExLabel.numberOfLines = 2
+        visibilityExLabelSetup()
+        //바람 세팅
+        windSpeedLabel.text = "\(currentWeatherWindSpeed)m/s"
+        windExLabel.numberOfLines = 2
+        windDirectionLabelSetup()
         //체감온도 세팅
         apparentTemperatureLabel.text = "\(currentWeatherApparentTemperature)º"
         apparentTemperatureExLabel.numberOfLines = 2
+        apparentTemperatureExSetup()
         //습도 세팅
         humidityLabel.text = "\(currentWeatherHumidity)%"
+        humidityExLabel.numberOfLines = 2
+        humidityExLabel.text = "현재 이슬점이 \(currentWeatherDewPoint)º입니다."
         //강수확률 세팅
         precipitationChanceLabel.text = "\(precipitation)%"
         precipitaionChanceExLabel.numberOfLines = 2
@@ -156,9 +174,21 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate, UICol
         print("체감온도: \(currentWeatherApparentTemperature)")
         print("습도: \(currentWeatherHumidity)")
         print("강수확률: \(precipitation)")
-        
-        uvIndexProgressSetup()
-        ApparentTemperatureExSetup()
+    }
+    //가시거리 레이블 세팅
+    func visibilityExLabelSetup() {
+        switch currentWeatherVisibility {
+        case ..<1:
+            visibilityExLabel.text = "가시거리가 안개의 영향을 받습니다."
+        case 2...10:
+            visibilityExLabel.text = "가시거리가 실안개의 영향을 받습니다."
+        case 10...20:
+            visibilityExLabel.text = "가시거리가 좋은 상태입니다."
+        case 20...:
+            visibilityExLabel.text = "가시거리가 매우좋은 상태입니다."
+        default:
+            break
+        }
     }
     //uv 프로그레스뷰 세팅
     func uvIndexProgressSetup() {
@@ -187,8 +217,63 @@ class CurrentWeatherViewController: UIViewController, UISearchBarDelegate, UICol
             break
         }
     }
+    //바람 방향 설정
+    func windDirectionLabelSetup() {
+        switch currentWeatherWinddirection {
+        case "east":
+            windDirectionLabel.text = "동풍"
+            windExLabel.text = "동쪽에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "eastNortheast":
+            windDirectionLabel.text = "동북동풍"
+            windExLabel.text = "동쪽과 북동쪽 사이에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "eastSoutheast":
+            windDirectionLabel.text = "동남동풍"
+            windExLabel.text = "동쪽과 남동쪽 사이에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "north":
+            windDirectionLabel.text = "북풍"
+            windExLabel.text = "북쪽에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "northNortheast":
+            windDirectionLabel.text = "북북동풍"
+            windExLabel.text = "북쪽과 북동쪽 사이에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "northNorthwest":
+            windDirectionLabel.text = "북북서풍"
+            windExLabel.text = "북쪽과 북서쪽 사이에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "northeast":
+            windDirectionLabel.text = "북동풍"
+            windExLabel.text = "북동쪽에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "northwest":
+            windDirectionLabel.text = "북서풍"
+            windExLabel.text = "북서쪽에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "south":
+            windDirectionLabel.text = "남풍"
+            windExLabel.text = "남쪽에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "southSoutheast":
+            windDirectionLabel.text = "남남동풍"
+            windExLabel.text = "남쪽과 남동쪽 사이에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "southSouthwest":
+            windDirectionLabel.text = "남남서풍"
+            windExLabel.text = "남쪽과 남서쪽 사이에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "southeast":
+            windDirectionLabel.text = "남동풍"
+            windExLabel.text = "남동쪽에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "southwest":
+            windDirectionLabel.text = "남서풍"
+            windExLabel.text = "남서쪽에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "west":
+            windDirectionLabel.text = "서풍"
+            windExLabel.text = "서쪽에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "westNorthwest":
+            windDirectionLabel.text = "서북서풍"
+            windExLabel.text = "서쪽과 북서쪽 사이에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        case "westSouthwest":
+            windDirectionLabel.text = "서남서풍"
+            windExLabel.text = "서쪽과 남서쪽 사이에서 \(currentWeatherWindSpeed)m/s로 바람이 붑니다."
+        default:
+            break
+        }
+    }
     //체감온도 ex레이블 세팅
-    func ApparentTemperatureExSetup() {
+    func apparentTemperatureExSetup() {
         if currentWeatherTemp == currentWeatherApparentTemperature {
             apparentTemperatureExLabel.text = "실제 온도와 동일하게 느껴집니다."
         } else if currentWeatherTemp > currentWeatherApparentTemperature {
--- weatherProject/ViewController.swift
+++ weatherProject/ViewController.swift
@@ -45,6 +45,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
     let seoul = CLLocation(latitude: 37.5666, longitude: 126.9784)
     //날씨 데이터 저장
     var weather: Weather?
+    //날씨 컨디션 저장
+    var currentWeatherCondition = ""
     //시간당 온도
     var hourWeatherTempArray: [String] = []
     var hourWeatherSymbol: [String] = []
@@ -62,12 +64,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
     //자외선 지수
     var currentWeatherUvIndex: Int = 0
     //풍속, 풍향
-    var currentWeatherWindSpeed = ""
+    var currentWeatherWindSpeed: Int = 0
     var currentWeatherWinddirection = ""
     //체감온도
     var currentWeatherApparentTemperature: Int = 0
     //습도
     var currentWeatherHumidity: Int = 0
+    //이슬점
+    var currentWeatherDewPoint: Int = 0
     //강수량
     var precipitation: Int = 0
     
@@ -76,7 +80,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
         super.viewDidLoad()
         //ui함수
         setupUI()
-        setupWeatherUI()
         
         //view를 클릭 가능하도록 설정
         self.firstview.isUserInteractionEnabled = true
@@ -112,12 +115,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
                         self.weekWeatherSymbolArray.append(self.weather!.dailyForecast[i].symbolName)
                     }
                     print(self.weekWeatherSymbolArray)
+                    print(self.weather!.currentWeather.condition)
+                    print(self.weather!.currentWeather.symbolName)
+                    print(self.weather!.currentWeather.condition)
                     //현재시간 불러오기
                     let formatter = DateFormatter()
                     formatter.locale = Locale(identifier: "ko")
                     formatter.dateFormat = "HH"
                     let currentHour = Int(formatter.string(from: Date()))!
-                    print(currentHour)
                     //시간당 날씨 받아오기
                     for j in (currentHour + 2)...(currentHour + 25) {
                         self.hourWeatherTempArray.append("\(Int(round(self.weather!.hourlyForecast[j].temperature.value)))º")
@@ -125,23 +130,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
                     }
 
                     //현재 날씨 받아오기
+                    self.currentWeatherCondition = self.weather!.currentWeather.condition.rawValue
                     self.dailyWeatherMaxTemp = Int(round(self.weather!.dailyForecast[0].highTemperature.value))
                     self.dailyWeatherMinTemp = Int(round(self.weather!.dailyForecast[0].lowTemperature.value))
                     self.currentWeatherSymbol = self.weather!.currentWeather.symbolName
                     self.currentWeatherTemp = Int(round(self.weather!.currentWeather.temperature.value))
                     self.currentWeatherVisibility = Int(round(self.weather!.currentWeather.visibility.value / 1000))
                     self.currentWeatherUvIndex = self.weather!.currentWeather.uvIndex.value
-                    self.currentWeatherWindSpeed = "\(self.weather!.currentWeather.wind.speed.value)"
-                    self.currentWeatherWinddirection = "\(self.weather!.currentWeather.wind.direction.value)"
+                    self.currentWeatherWindSpeed = Int(round(self.weather!.currentWeather.wind.speed.value / 3.6))
+                    self.currentWeatherWinddirection = "\(self.weather!.currentWeather.wind.compassDirection.rawValue)"
+                    print(self.currentWeatherWinddirection)
                     self.currentWeatherApparentTemperature = Int(round(self.weather!.currentWeather.apparentTemperature.value))
                     self.currentWeatherHumidity = Int(round(self.weather!.currentWeather.humidity * 100))
                     self.precipitation = Int(round(self.weather!.dailyForecast[0].precipitationChance * 100))
+                    self.currentWeatherDewPoint = Int(round(self.weather!.currentWeather.dewPoint.value))
                     
                     //ui세팅
+                    self.currentWeatherConditionKO()
                     self.setWeatherUI()
                     self.weekWeatherTableView.reloadData()
-                    
-                    print(self.weather!.currentWeather.condition.description)
                 } catch {
                     print("error")
                 }
@@ -182,12 +189,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
     
     //오늘 날씨뷰 ui
     func setupWeatherUI() {
-        //오늘 날짜 표시
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy년 MM월 d일 (E)"
-        weatherDateLabel.text = formatter.string(from: Date())
         
-        weatherRegionLabel.text = "서울"
     }
     
     //첫번째 뷰를 눌렀을 때
@@ -206,6 +208,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showCurrentWeatherView" {
             guard let vc = segue.destination as? CurrentWeatherViewController else { return }
+            vc.currentWeatherCondition = self.currentWeatherCondition
             vc.hourWeatherTempArray = self.hourWeatherTempArray
             vc.weekWeatherSymbolArray = self.weekWeatherSymbolArray
             vc.weekWeatherMaxTempArray = self.weekWeatherMaxTempArray
@@ -221,6 +224,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
             vc.currentWeatherWinddirection = self.currentWeatherWinddirection
             vc.currentWeatherApparentTemperature = self.currentWeatherApparentTemperature
             vc.currentWeatherHumidity = self.currentWeatherHumidity
+            vc.currentWeatherDewPoint = self.currentWeatherDewPoint
             vc.precipitation = self.precipitation
         }
     }
@@ -231,9 +235,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
     }
     //현재 온도 세팅
     private func setWeatherUI() {
-        //        weatherTempLabel.text = "\(Int(main!.temp - 273.15))ºC"
-        //        weatherMaxTempLabel.text = "최고:\(Int(main!.temp_max - 273.15))ºC"
-        //        weatherMinTempLabel.text = "최저:\(Int(main!.temp_min - 273.15))ºC"
+        //오늘 날짜 표시
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 d일 (E)"
+        weatherDateLabel.text = formatter.string(from: Date())
+        weatherRegionLabel.text = "서울"
+        
+        weatherLabel.text = self.currentWeatherCondition
         //현재온도
         weatherTempLabel.text = "\(self.currentWeatherTemp)º"
         //최고온도
@@ -242,7 +250,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
         weatherMinTempLabel.text = "최저:\(self.dailyWeatherMinTemp)º"
         //심볼네임
         weatherImage.image = UIImage(named: self.currentWeatherSymbol)
-        print(weather!.currentWeather.symbolName)
     }
     
     //테이블뷰 셀의 숫자
@@ -273,6 +280,82 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDa
         }
             
         return cell
+    }
+    
+    //날씨 컨디션 케이스별 번역
+    func currentWeatherConditionKO() {
+        switch currentWeatherCondition {
+        case "blowingDust":
+            currentWeatherCondition = "먼지"
+        case "clear":
+            currentWeatherCondition = "맑음"
+        case "cloudy":
+            currentWeatherCondition = "흐림"
+        case "foggy":
+            currentWeatherCondition = "안개"
+        case "haze":
+            currentWeatherCondition = "연무"
+        case "mostlyClear":
+            currentWeatherCondition = "대체로 맑음"
+        case "mostlyCloudy":
+            currentWeatherCondition = "대체로 흐림"
+        case "partlyCloudy":
+            currentWeatherCondition = "한때 흐림"
+        case "smoky":
+            currentWeatherCondition = "연기"
+        case "breezy":
+            currentWeatherCondition = "미풍"
+        case "windy":
+            currentWeatherCondition = "바람"
+        case "drizzle":
+            currentWeatherCondition = "이슬비"
+        case "heavyRain":
+            currentWeatherCondition = "폭우"
+        case "isolatedThunderstorms":
+            currentWeatherCondition = "외딴뇌우"
+        case "rain":
+            currentWeatherCondition = "비"
+        case "sunShowers":
+            currentWeatherCondition = "소나기"
+        case "scatteredThunderstorms":
+            currentWeatherCondition = "흩어진뇌우"
+        case "strongStorms":
+            currentWeatherCondition = "폭풍"
+        case "thunderstorms":
+            currentWeatherCondition = "뇌우"
+        case "frigid":
+            currentWeatherCondition = "몹시 추움"
+        case "hail":
+            currentWeatherCondition = "우박"
+        case "hot":
+            currentWeatherCondition = "더움"
+        case "flurries":
+            currentWeatherCondition = "돌풍"
+        case "sleet":
+            currentWeatherCondition = "진눈깨비"
+        case "snow":
+            currentWeatherCondition = "눈"
+        case "sunFlurries":
+            currentWeatherCondition = "소낙눈"
+        case "wintryMix":
+            currentWeatherCondition = "눈, 비"
+        case "blizzard":
+            currentWeatherCondition = "눈보라"
+        case "blowingSnow":
+            currentWeatherCondition = "날린눈"
+        case "freezingDrizzle":
+            currentWeatherCondition = "언 진눈깨비"
+        case "freezingRain":
+            currentWeatherCondition = "얼어붙은 비"
+        case "heavySnow":
+            currentWeatherCondition = "폭설"
+        case "hurricane":
+            currentWeatherCondition = "허리케인"
+        case "tropicalStorm":
+            currentWeatherCondition = "열대 폭풍"
+        default:
+            break
+        }
     }
     
 }
